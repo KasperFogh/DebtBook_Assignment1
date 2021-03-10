05391905ba96d98089fe76a1de4eb53c9998a846
RuneHell <60700419+RuneHell@users.noreply.github.com>
2021-03-10 13:07:12 +0100

    first binding

--- ViewModel.cs
+++ ViewModel.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Mvvm;
+using Prism.Commands;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace DebtBook
 {
-    class ViewModel
+    public class ViewModel:BindableBase
     {
+        public ViewModel()
+        {
+
+        }
+        #region Commands
+        ICommand _exitCommand;
+        public ICommand ExitCommand
+        {
+            get
+            {
+                return _exitCommand ?? (_exitCommand = new DelegateCommand(() =>
+                 {
+                     App.Current.MainWindow.Close();
+                 }));
+            }
+        }
+        #endregion
     }
 
 }
